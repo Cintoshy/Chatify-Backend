fb5e5ca6a677321defefe6b8a92b412ab3d9484d
Mark Louis R. Odavar <122847155+Cintoshy@users.noreply.github.com>
2024-05-10 17:14:55 +0800

    feat: create a controllller for channel

---
+++ controller/chat.js
@@ -0,0 +1,37 @@
+const Channels = require("../model/channelModel");
+const Messages = require("../model/messageModel");
+
+exports.getChannel = async (req, res) => {
+  try {
+    const channelList = await Channels.find().populate({
+      path: "lastMessage",
+      populate: {
+        path: "sender",
+        select: "name email",
+      },
+    });
+
+    if (!channelList) {
+      return res.status(404).json({ message: "Channel not found" });
+    }
+
+    res.status(200).json(channelList);
+  } catch (err) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+exports.getMessage = async (req, res) => {
+  try {
+    // const userId = req.params.id;
+    const messages = await Messages.find();
+
+    if (!messages) {
+      return res.status(404).json({ message: "Channel not found" });
+    }
+
+    res.status(200).json(messages);
+  } catch (err) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
