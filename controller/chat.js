25496371deffe1aefe32ce25b70ad6a51462fe57
Mark Louis R. Odavar <122847155+Cintoshy@users.noreply.github.com>
2024-05-11 22:16:59 +0800

    feat: added filter get channels

--- controller/chat.js
+++ controller/chat.js
@@ -2,20 +2,46 @@ const Channels = require("../model/channelModel");
 const Messages = require("../model/messageModel");
 
 exports.getChannel = async (req, res) => {
+  const memberId = req.params.id;
   try {
-    const channelList = await Channels.find().populate([
-      { path: "lastMessage" },
+    const channelList = await Channels.find({
+      members: { $in: [memberId] },
+    }).populate([
+      {
+        path: "lastMessage",
+      },
       { path: "members", select: "name email" },
     ]);
-    if (!channelList) {
+
+    // Check if channelList is empty or not
+    if (channelList.length === 0) {
       return res.status(404).json({ message: "Channel not found" });
     }
+
+    channelList.forEach((channel) => {
+      if (channel.lastMessage.length > 10) {
+        channel.lastMessage = channel.lastMessage.slice(0, 10);
+      }
+    });
 
     res.status(200).json(channelList);
   } catch (err) {
     res.status(500).json({ error: "Internal server error" });
   }
 };
+// exports.getChannel = async (req, res) => {
+//   const { firstId, secondId } = req.params;
+//   try {
+//     const channelList = await Channels.findOne({
+//       members: { $all: [firstId, secondId] },
+//     });
+//     console.log(channelList);
+
+//     res.status(200).json(channelList);
+//   } catch (err) {
+//     res.status(500).json({ error: "Internal server error" });
+//   }
+// };
 
 exports.getMessage = async (req, res) => {
   try {
--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const chatController = require("../controller/chat");
 
-router.get("/", chatController.getChannel);
+router.get("/:id", chatController.getChannel);
 
 module.exports = router;
